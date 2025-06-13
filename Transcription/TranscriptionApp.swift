a93f296bd40321b0db101658921ee62ae580fddc
Farasatullah <50820479+Farasatullah@users.noreply.github.com>
2025-06-13 12:14:27 +0500

    Initial Commit

---
+++ Transcription/TranscriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriptionApp.swift
+//  Transcription
+//
+//  Created by Farasat's_MacBook_Pro on 13/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TranscriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
